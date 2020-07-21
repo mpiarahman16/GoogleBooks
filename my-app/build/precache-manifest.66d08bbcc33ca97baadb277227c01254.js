edd44b66a297462fe8d73243400b5277313b9b03
mpiarahman16 <mpiarahman@gmail.com>
2020-07-20 22:19:58 -0400

    Final HW Yay

---
+++ my-app/build/precache-manifest.66d08bbcc33ca97baadb277227c01254.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d4a16df4ab7d8ec9bdbac10f27a323f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a49f700189a3901d5e18",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2fce961624100ea0849a",
+    "url": "/static/js/2.a20cc6b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a20cc6b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a49f700189a3901d5e18",
+    "url": "/static/js/main.2b2c177d.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
